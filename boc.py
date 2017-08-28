22ef44175f48686eea9fe6f0297901df0d25036e
Aidan B <aidanrichbennett+github@gmail.com>
2017-08-28 13:49:13 -0400

    Fixes for markdown changes and writeBocf()

--- boc.py
+++ boc.py
@@ -14,15 +14,15 @@ def readBoc():
         events = []
         for event in eventsraw:
             if(event != ''):
-                event = event.split(".", 1)[1]
-                events.append([event[4:6], event[10:]])
+                #event = event.split(".", 1)[1]
+                events.append([event[6:10], event[14:]])
         boc.append(events)
     #boc[-1][-1][-1] = boc[-1][-1][-1][:-1]
 
 def initBoc():
     bocf.write("# Book of Centuries\n\n")
     for century in range(21):
-        bocf.write("## Century: " + str(century) + "00s:\n\n")
+        bocf.write("## Century: " + str(century).zfill(2) + "00s:\n\n")
 
 def getCentury(century):
     return boc[century - 1]
@@ -35,14 +35,14 @@ def getEvent(century, event):
 
 def editEvent(century, event, year, content):
     try:
-        boc[century - 1][event - 1][0] = str(year)
+        boc[century - 1][event - 1][0] = str(century).zfill(2) + str(year)
         boc[century - 1][event - 1][1] = str(content)
         return True
     except:
         return False
 
 def newEvent(century, year, content):
-    boc[century - 1] = sorted(boc[century - 1] + [[str(year), str(content)]])
+    boc[century - 1] = sorted(boc[century - 1] + [[str(century).zfill(2) + str(year), str(content)]])
 
 def deleteEvent(century, event):
     try:
@@ -50,6 +50,18 @@ def deleteEvent(century, event):
         return True
     except:
         return False
+
+def writeBoc():
+    towrite = "# Book of Centuries\n\n"
+    cennum = 0
+    for century in boc:
+        towrite += "## Century: " + str(cennum).zfill(2) + "00s:\n"
+        for event in century:
+            towrite += "  - **" + event[0] + ":** " + event[1] + "\n"
+        towrite += "\n"
+        cennum += 1
+    bocf.truncate(0)
+    bocf.write(towrite)
 
 openBoc()
 bocraw = bocf.read()
@@ -59,10 +71,6 @@ if(len(bocraw) < 22):
     readBoc()
 else:
     initBoc()
-
-
-getCentury(1)
-getEvent(1, 3)
-editEvent(1, 1, 46, "one")
-newEvent(1, 62, "one and a half")
-deleteEvent(1, 3)
+boc
+writeBoc()
+bocf.close()
